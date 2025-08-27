ad77eae4546cd6f895a18692be4b0a286c747433
Arthur <aheracliocunha@gmail.com>
2025-08-27 16:55:51 -0300

    refactor: simplifica o metodo exibirTabuleiro

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,15 +1,23 @@
 public class Tabuleiro{
     public void exibirTabuleiro(String[][] tabuleiro) {
-        for(int i = 0; i < tabuleiro[0].length; i++){
-            System.out.print(tabuleiro[0][i]);
-        }
-        System.out.println("\n---------");
-        for(int j = 0; j < tabuleiro[1].length; j++){
-            System.out.print(tabuleiro[1][j]);
-        }
-        System.out.println("\n---------");
-        for(int k = 0; k < tabuleiro[2].length; k++){
-            System.out.print(tabuleiro[2][k]);
+        for(int i = 0; i < tabuleiro.length; i++){
+            System.out.println(String.join(" | ", tabuleiro[i]));
+
+            if(i < tabuleiro.length - 1){
+                System.out.println("---------");
+            }
         }
     }
 }
+
+// for(int i = 0; i < tabuleiro[0].length; i++){
+//     System.out.print(tabuleiro[0][i]);
+// }
+// System.out.println("\n---------");
+// for(int j = 0; j < tabuleiro[1].length; j++){
+//     System.out.print(tabuleiro[1][j]);
+// }
+// System.out.println("\n---------");
+// for(int k = 0; k < tabuleiro[2].length; k++){
+//     System.out.print(tabuleiro[2][k]);
+// }
