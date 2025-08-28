563979768b6d03d7948bbc7964ee9b6cc319f901
Arthur <aheracliocunha@gmail.com>
2025-08-28 20:36:42 -0300

    refactor: modifica o retorno de metodo verificarVitoria

--- Vitoria.java
+++ Vitoria.java
@@ -1,5 +1,5 @@
 public class Vitoria {
-    public String verificarVitoria(String[][] tabuleiro){
+    public boolean verificarVitoria(String[][] tabuleiro){
         if ((tabuleiro[0][0].equals('X') && tabuleiro[0][2].equals('X') && tabuleiro[0][4].equals('X')) |
             (tabuleiro[1][0].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[1][4].equals('X')) |
             (tabuleiro[2][0].equals('X') && tabuleiro[2][2].equals('X') && tabuleiro[2][4].equals('X')) |
@@ -8,7 +8,7 @@ public class Vitoria {
             (tabuleiro[0][4].equals('X') && tabuleiro[1][4].equals('X') && tabuleiro[2][4].equals('X')) |
             (tabuleiro[0][0].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][4].equals('X')) |
             (tabuleiro[0][4].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][0].equals('X'))){
-                return "Jogador X";
+                return true;
             }
         else if((tabuleiro[0][0].equals('O') && tabuleiro[0][2].equals('O') && tabuleiro[0][4].equals('O')) |
                 (tabuleiro[1][0].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[1][4].equals('O')) |
@@ -18,10 +18,10 @@ public class Vitoria {
                 (tabuleiro[0][4].equals('O') && tabuleiro[1][4].equals('O') && tabuleiro[2][4].equals('O')) |
                 (tabuleiro[0][0].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][4].equals('O')) |
                 (tabuleiro[0][4].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][0].equals('O'))){
-                    return "Jogador O";
+                    return true;
                 }
         else {
-            return "Empate";
+            return false;
         }
     }
 }
