6d765cc9d7849647b4cbc2b00187f4039b7a2802
Arthur <aheracliocunha@gmail.com>
2025-08-28 22:08:58 -0300

    fix: troca as aspas simples por aspas duplas

--- Vitoria.java
+++ Vitoria.java
@@ -1,22 +1,22 @@
 public class Vitoria {
     public boolean verificarVitoria(String[][] tabuleiro) {
-        if ((tabuleiro[0][0].equals('X') && tabuleiro[0][1].equals('X') && tabuleiro[0][2].equals('X')) |
-                (tabuleiro[1][0].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[1][2].equals('X')) |
-                (tabuleiro[2][0].equals('X') && tabuleiro[2][1].equals('X') && tabuleiro[2][2].equals('X')) |
-                (tabuleiro[0][0].equals('X') && tabuleiro[1][0].equals('X') && tabuleiro[2][0].equals('X')) |
-                (tabuleiro[0][1].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][1].equals('X')) |
-                (tabuleiro[0][2].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][2].equals('X')) |
-                (tabuleiro[0][0].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][2].equals('X')) |
-                (tabuleiro[0][2].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][0].equals('X'))) {
+        if ((tabuleiro[0][0].equals("X") && tabuleiro[0][1].equals("X") && tabuleiro[0][2].equals("X")) ||
+                (tabuleiro[1][0].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[1][2].equals("X")) |
+                (tabuleiro[2][0].equals("X") && tabuleiro[2][1].equals("X") && tabuleiro[2][2].equals("X")) |
+                (tabuleiro[0][0].equals("X") && tabuleiro[1][0].equals("X") && tabuleiro[2][0].equals("X")) |
+                (tabuleiro[0][1].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][1].equals("X")) |
+                (tabuleiro[0][2].equals("X") && tabuleiro[1][2].equals("X") && tabuleiro[2][2].equals("X")) |
+                (tabuleiro[0][0].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][2].equals("X")) |
+                (tabuleiro[0][2].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][0].equals("X"))) {
             return true;
-        } else if ((tabuleiro[0][0].equals('O') && tabuleiro[0][1].equals('O') && tabuleiro[0][2].equals('O')) |
-                (tabuleiro[1][0].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[1][2].equals('O')) |
-                (tabuleiro[2][0].equals('O') && tabuleiro[2][1].equals('O') && tabuleiro[2][2].equals('O')) |
-                (tabuleiro[0][0].equals('O') && tabuleiro[1][0].equals('O') && tabuleiro[2][0].equals('O')) |
-                (tabuleiro[0][1].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][1].equals('O')) |
-                (tabuleiro[0][2].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][2].equals('O')) |
-                (tabuleiro[0][0].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][2].equals('O')) |
-                (tabuleiro[0][2].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][0].equals('O'))) {
+        } else if ((tabuleiro[0][0].equals("O") && tabuleiro[0][1].equals("O") && tabuleiro[0][2].equals("O")) |
+                (tabuleiro[1][0].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[1][2].equals("O")) |
+                (tabuleiro[2][0].equals("O") && tabuleiro[2][1].equals("O") && tabuleiro[2][2].equals("O")) |
+                (tabuleiro[0][0].equals("O") && tabuleiro[1][0].equals("O") && tabuleiro[2][0].equals("O")) |
+                (tabuleiro[0][1].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][1].equals("O")) |
+                (tabuleiro[0][2].equals("O") && tabuleiro[1][2].equals("O") && tabuleiro[2][2].equals("O")) |
+                (tabuleiro[0][0].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][2].equals("O")) |
+                (tabuleiro[0][2].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][0].equals("O"))) {
             return true;
         } else {
             return false;
