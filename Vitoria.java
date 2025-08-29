89eb625e673548cb9f5276e8f6577d0f803b4677
Arthur <aheracliocunha@gmail.com>
2025-08-28 21:45:49 -0300

    fix: corrige o index do array

--- Vitoria.java
+++ Vitoria.java
@@ -1,26 +1,24 @@
 public class Vitoria {
-    public boolean verificarVitoria(String[][] tabuleiro){
-        if ((tabuleiro[0][0].equals('X') && tabuleiro[0][2].equals('X') && tabuleiro[0][4].equals('X')) |
-            (tabuleiro[1][0].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[1][4].equals('X')) |
-            (tabuleiro[2][0].equals('X') && tabuleiro[2][2].equals('X') && tabuleiro[2][4].equals('X')) |
-            (tabuleiro[0][0].equals('X') && tabuleiro[1][0].equals('X') && tabuleiro[2][0].equals('X')) |
-            (tabuleiro[0][2].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][2].equals('X')) |
-            (tabuleiro[0][4].equals('X') && tabuleiro[1][4].equals('X') && tabuleiro[2][4].equals('X')) |
-            (tabuleiro[0][0].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][4].equals('X')) |
-            (tabuleiro[0][4].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][0].equals('X'))){
-                return true;
-            }
-        else if((tabuleiro[0][0].equals('O') && tabuleiro[0][2].equals('O') && tabuleiro[0][4].equals('O')) |
-                (tabuleiro[1][0].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[1][4].equals('O')) |
-                (tabuleiro[2][0].equals('O') && tabuleiro[2][2].equals('O') && tabuleiro[2][4].equals('O')) |
+    public boolean verificarVitoria(String[][] tabuleiro) {
+        if ((tabuleiro[0][0].equals('X') && tabuleiro[0][1].equals('X') && tabuleiro[0][2].equals('X')) |
+                (tabuleiro[1][0].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[1][2].equals('X')) |
+                (tabuleiro[2][0].equals('X') && tabuleiro[2][1].equals('X') && tabuleiro[2][2].equals('X')) |
+                (tabuleiro[0][0].equals('X') && tabuleiro[1][0].equals('X') && tabuleiro[2][0].equals('X')) |
+                (tabuleiro[0][1].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][1].equals('X')) |
+                (tabuleiro[0][2].equals('X') && tabuleiro[1][2].equals('X') && tabuleiro[2][2].equals('X')) |
+                (tabuleiro[0][0].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][2].equals('X')) |
+                (tabuleiro[0][2].equals('X') && tabuleiro[1][1].equals('X') && tabuleiro[2][0].equals('X'))) {
+            return true;
+        } else if ((tabuleiro[0][0].equals('O') && tabuleiro[0][1].equals('O') && tabuleiro[0][2].equals('O')) |
+                (tabuleiro[1][0].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[1][2].equals('O')) |
+                (tabuleiro[2][0].equals('O') && tabuleiro[2][1].equals('O') && tabuleiro[2][2].equals('O')) |
                 (tabuleiro[0][0].equals('O') && tabuleiro[1][0].equals('O') && tabuleiro[2][0].equals('O')) |
+                (tabuleiro[0][1].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][1].equals('O')) |
                 (tabuleiro[0][2].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][2].equals('O')) |
-                (tabuleiro[0][4].equals('O') && tabuleiro[1][4].equals('O') && tabuleiro[2][4].equals('O')) |
-                (tabuleiro[0][0].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][4].equals('O')) |
-                (tabuleiro[0][4].equals('O') && tabuleiro[1][2].equals('O') && tabuleiro[2][0].equals('O'))){
-                    return true;
-                }
-        else {
+                (tabuleiro[0][0].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][2].equals('O')) |
+                (tabuleiro[0][2].equals('O') && tabuleiro[1][1].equals('O') && tabuleiro[2][0].equals('O'))) {
+            return true;
+        } else {
             return false;
         }
     }
