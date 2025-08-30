48dc7299df86df37cd58b157301b7ab9cd281c6b
Arthur <aheracliocunha@gmail.com>
2025-08-30 20:37:39 -0300

    refactor: simplifica o metodo verificarVitoria

--- Vitoria.java
+++ Vitoria.java
@@ -1,25 +1,23 @@
 public class Vitoria {
-    public boolean verificarVitoria(String[][] tabuleiro) {
-        if ((tabuleiro[0][0].equals("X") && tabuleiro[0][1].equals("X") && tabuleiro[0][2].equals("X")) ||
-                (tabuleiro[1][0].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[1][2].equals("X")) ||
-                (tabuleiro[2][0].equals("X") && tabuleiro[2][1].equals("X") && tabuleiro[2][2].equals("X")) ||
-                (tabuleiro[0][0].equals("X") && tabuleiro[1][0].equals("X") && tabuleiro[2][0].equals("X")) ||
-                (tabuleiro[0][1].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][1].equals("X")) ||
-                (tabuleiro[0][2].equals("X") && tabuleiro[1][2].equals("X") && tabuleiro[2][2].equals("X")) ||
-                (tabuleiro[0][0].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][2].equals("X")) ||
-                (tabuleiro[0][2].equals("X") && tabuleiro[1][1].equals("X") && tabuleiro[2][0].equals("X"))) {
-            return true;
-        } else if ((tabuleiro[0][0].equals("O") && tabuleiro[0][1].equals("O") && tabuleiro[0][2].equals("O")) ||
-                (tabuleiro[1][0].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[1][2].equals("O")) ||
-                (tabuleiro[2][0].equals("O") && tabuleiro[2][1].equals("O") && tabuleiro[2][2].equals("O")) ||
-                (tabuleiro[0][0].equals("O") && tabuleiro[1][0].equals("O") && tabuleiro[2][0].equals("O")) ||
-                (tabuleiro[0][1].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][1].equals("O")) ||
-                (tabuleiro[0][2].equals("O") && tabuleiro[1][2].equals("O") && tabuleiro[2][2].equals("O")) ||
-                (tabuleiro[0][0].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][2].equals("O")) ||
-                (tabuleiro[0][2].equals("O") && tabuleiro[1][1].equals("O") && tabuleiro[2][0].equals("O"))) {
-            return true;
-        } else {
-            return false;
+    public boolean verificarVitoria(String[][] tabuleiro, String simbolo){
+        for(int i = 0; i < tabuleiro.length; i++){
+            // verifica se as linhas 
+            if(tabuleiro[i][0].equals(simbolo) && tabuleiro[i][1].equals(simbolo) && tabuleiro[i][2]. equals(simbolo)){
+                return true;
+            }
+            // verifica se as colunas
+            if(tabuleiro[0][i].equals(simbolo) && tabuleiro[1][i].equals(simbolo) && tabuleiro[2][i].equals(simbolo)){
+                return true;
+            }
         }
+        // verifica as diagonais
+        if(tabuleiro[0][0].equals(simbolo) && tabuleiro[1][1].equals(simbolo) && tabuleiro[2][2].equals(simbolo)){
+            return true;
+        }
+        if(tabuleiro[0][2].equals(simbolo) && tabuleiro[1][1].equals(simbolo) && tabuleiro[2][0].equals(simbolo)){
+            return true;
+        }
+        // caso não haja vitória 
+        return false;
     }
 }
