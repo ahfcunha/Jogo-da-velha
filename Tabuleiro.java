6872435d7c14685fab701404d2445178ae7b0788
Arthur <aheracliocunha@gmail.com>
2025-08-31 19:48:06 -0300

    feat: implementa verificacao na entrada do número de jogadores

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Tabuleiro{
     Scanner s = new Scanner(System.in);
+    boolean numeroValido;
+    int numeroJogadores;
     
 
     public void exibirTabuleiro(String[][] tabuleiro) {
@@ -16,19 +18,14 @@ public class Tabuleiro{
 
     public int exibirMenu(){
         System.out.println("***JOGO DA VELHA***");
-        System.out.println("\nNúmeoro de jogadores (1 ou 2): ");
-        return s.nextInt();
+        numeroValido = false;
+        while(!numeroValido){
+            System.out.println("\nNúmero de jogadores (1 ou 2): ");
+            numeroJogadores = s.nextInt();
+            if(numeroJogadores == 1 || numeroJogadores == 2){
+                numeroValido = true;
+            }
+        }
+        return numeroJogadores;
     }
 }
-
-// for(int i = 0; i < tabuleiro[0].length; i++){
-//     System.out.print(tabuleiro[0][i]);
-// }
-// System.out.println("\n---------");
-// for(int j = 0; j < tabuleiro[1].length; j++){
-//     System.out.print(tabuleiro[1][j]);
-// }
-// System.out.println("\n---------");
-// for(int k = 0; k < tabuleiro[2].length; k++){
-//     System.out.print(tabuleiro[2][k]);
-// }
